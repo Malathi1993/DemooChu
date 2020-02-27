220d40e370697f44198ac337652a23c13d15703d
Malathi <57785954+Malathi1993@users.noreply.github.com>
2020-02-27 17:50:08 +0530

    addRemoveTitle Bar

--- app/src/main/java/com/example/demoo/SpalashActivity.java
+++ app/src/main/java/com/example/demoo/SpalashActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ProgressBar;
 
 public class SpalashActivity extends AppCompatActivity {
@@ -12,6 +14,8 @@ public class SpalashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //Remove Title
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_spalash);
         mProgress = (ProgressBar) findViewById(R.id.progressBar);
       /*  new Handler().postDelayed(new Runnable() {
